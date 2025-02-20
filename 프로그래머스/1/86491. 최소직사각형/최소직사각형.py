24f5a907898e37008b5f932fa331574c80469067
Gwangsu An <108314208+Ansu-dev@users.noreply.github.com>
2025-02-20 14:38:39 +0900

    [level 1] Title: 최소직사각형, Time: 7.13 ms, Memory: 12.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,11 @@
+def solution(sizes):
+    answer = 0
+    # 명함을 회전시킬 수 있으므로, 각 명함에서 더 작은 수는 세로, 더 큰수는 가로로 맞춰줌
+    rotated = [sorted(card) for card in sizes] # O(1) => 명함 한 개는 길이가 2인 배열이므로, 정렬에 걸리는 시간은 상수 시간
+
+    # 각 인덱스에서 가장 큰 수들만 뽑아서 명함지갑을 생성
+    max_width = max(row[0] for row in rotated) # O(n)
+    max_height = max(row[1] for row in rotated) # O(n)
+
+    answer = max_width * max_height
+    return answer
